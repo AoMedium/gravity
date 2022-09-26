ef551073545e84ca3b090fc1b803fd2aca18dfec
AoMedium <75078332+AoMedium@users.noreply.github.com>
2022-09-26 23:19:37 +1300

    rename public to out; move index.html to root

--- out/utils.js
+++ out/utils.js
@@ -1,57 +1,25 @@
 //import systemJson from './json/systems.json';
-import { Vector2, Entity } from './models.js';
-
+import { Vector2 } from './models.js';
 const NON_ZERO_FACTOR = 0.00001;
 const G = 0.1;
-
 export class Calculations {
-    public static calculateAcceleration(displacement: Vector2, mass: number): Vector2 {
-        let direction: Vector2 = displacement.normalized();
+    static calculateAcceleration(displacement, mass) {
+        let direction = displacement.normalized();
         if (displacement.equals(Vector2.zero())) {
             displacement.add(new Vector2(NON_ZERO_FACTOR, NON_ZERO_FACTOR));
         }
-        
-        let aMagnitude: number = -G * mass / (displacement.magnitude() * displacement.magnitude());
-    
+        let aMagnitude = -G * mass / (displacement.magnitude() * displacement.magnitude());
         return direction.scale(aMagnitude);
     }
 }
-
-interface System {
-    systemName: string;
-    systemCenter: string;
-    AU: number;
-    systemObjects: SystemObject[]; 
-}
-
-interface SystemObject {
-    name: string;
-    mass: number;
-    pos?: Vector2;
-    distance?: Vector2;
-    entityAttributes: EntityAttributes[];
-}
-
-interface EntityAttributes {
-    fixed?: boolean;
-    orbit?: boolean;
-    primaryColor: string;
-}
-
 export class SystemBuilder {
-    public static createSystem(systemName: string): Entity[] {
-        let system: Entity[] = [];
-
-        let parsedSystem: System = JSON.parse(json).systems;
-
-        
+    static createSystem(systemName) {
+        let system = [];
+        let parsedSystem = JSON.parse(json).systems;
         console.log(parsedSystem);
-
         return system;
     }
 }
-
-
 let json = `{
     "systems": [
         {
@@ -97,4 +65,5 @@ let json = `{
             ]
         }
     ]
-}`
+}`;
+//# sourceMappingURL=utils.js.map
