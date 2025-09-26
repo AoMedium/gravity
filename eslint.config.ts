a6799a10f13b634219b04b42a708c5fcd15b1f54
AoMedium <75078332+AoMedium@users.noreply.github.com>
2025-09-26 20:41:05 +1200

    detect react version

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,6 +12,11 @@ export default defineConfig([
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
